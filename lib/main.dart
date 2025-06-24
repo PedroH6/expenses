143de67f35107081842d59f651b6d030f5972506
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-06-24 15:40:54 -0300

    Item lista personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,16 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: _transaction.map((tr) {
-              return Card(child: Text(tr.title));
+              return Card(
+                child: Row(
+                  children: [
+                    Container(child: Text(tr.value.toString())),
+                    Column(
+                      children: [Text(tr.title), Text(tr.date.toString())],
+                    ),
+                  ],
+                ),
+              );
             }).toList(),
           ),
         ],
